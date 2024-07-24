57d9b615f2ab9d96d1ca8202c5a2916404a1c717
Shoaib-2005 <shaikshoaib620@gmil.com>
2024-07-24 18:56:38 +0530

    Second Commit

---
+++ MyFirstDjango/calc/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+def home(request):
+ return render(request,'home.html',{'name':'Mohammed Shoaib Hannan'})
+def add(request):
+ val1=int(request.POST['num1'])
+ val2=int(request.POST['num2'])
+ val3=val1+val2
+ return render(request,'result.html',{'result':val3})
+
+# Create your views here.
